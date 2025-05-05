2b3413d0ea7bf7841150595179d4459c961159f1
Mohsin Khan <209888552+BayHatter@users.noreply.github.com>
2025-05-05 14:13:29 +0500

    Update and rename eth.js to blast.js

--- pages/blast.js
+++ pages/blast.js
@@ -1,15 +1,15 @@
 import dynamic from 'next/dynamic';
 import WalletProvider from '../assets/WalletProvider';
 import TopMenu from '../assets/TopMenu';
-import Ethereum from '../assets/Ethereum';
+import BlastAsset from '../assets/BlastAsset';
 import Sidebar from '../assets/Sidebar';
 import Head from 'next/head';
 
-const EthereumWithNoSSR = dynamic(() => import('../assets/Ethereum'), {
+const BlastAssetWithNoSSR = dynamic(() => import('../assets/BlastAsset'), {
   ssr: false,
 });
 
-export default function Eth() {
+export default function Blast() {
   return (
     <div className="min-h-screen bg-white-100">
       <WalletProvider>
@@ -18,17 +18,17 @@ export default function Eth() {
             <TopMenu account={account} connectWallet={connectWallet} disconnectWallet={disconnectWallet} />
             
             <Head>
-              <title>Ethereum Token Creation Tool</title>
-              <meta name="description" content="Create and launch your own meme token on Ethereum" />
-              <meta property="og:title" content="Launch Your Meme Coin on Ethereum" />
-              <meta property="og:description" content="Build and deploy a meme coin instantly on Ethereum. Fast, secure, and 100% code-free." />
+              <title>Blast Token Creation Tool</title>
+              <meta name="description" content="Create and launch your own meme token on Blast" />
+              <meta property="og:title" content="Launch Your Meme Coin on Blast" />
+              <meta property="og:description" content="Build and deploy a meme coin instantly on Blast. Fast, secure, and 100% code-free." />
               <meta property="og:type" content="website" />
-              <meta property="og:url" content="/eth" />
+              <meta property="og:url" content="/blast" />
             </Head>
 
             <div className="container mx-auto p-4 max-w-7xl flex flex-col md:flex-row gap-4">
               <div className="flex-1">
-                <EthereumWithNoSSR provider={provider} account={account} connectWallet={connectWallet} />
+                <BlastAssetWithNoSSR provider={provider} account={account} connectWallet={connectWallet} />
               </div>
               <div className="md:w-[300px] flex-shrink-0">
                 <Sidebar />
